57ec3a3e51cd3888095614ea896fbcfd19d4da3e
Christopher Butcher <cbutcher@gmail.com>
2019-02-18 16:08:46 -0500

    simple raycaster

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,19 @@ extern crate sdl2;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 
 use std::time::Duration;
+
+use std::fs;
 
 pub fn main() -> Result<(), String> {
   let sdl_context = sdl2::init()?;
   let video_subsystem = sdl_context.video()?;
 
   let window = video_subsystem
-    .window("rust-sdl2 demo: Video", 512, 512)
+    .window("rust-sdl2 demo: Video", 1024, 512)
     .position_centered()
     .opengl()
     .build()
@@ -23,39 +24,27 @@ pub fn main() -> Result<(), String> {
   let texture_creator = canvas.texture_creator();
 
   let mut texture = texture_creator
-    .create_texture_streaming(PixelFormatEnum::RGB24, 512, 512)
+    .create_texture_streaming(PixelFormatEnum::RGB24, 1024, 512)
     .map_err(|e| e.to_string())?;
 
   // Raycaster code port
-  let win_w = 512;
+  let win_w = 1024;
   let win_h = 512;
 
   let map_w = 16;
   let map_h = 16;
 
-  let mut framebuffer: [u8; 512 * 512 * 3] = [0x00; 512 * 512 * 3];
+  let mut framebuffer: [u8; 512 * 1024 * 3] = [0x33; 512 * 1024 * 3];
 
-  let map = "0000222222220000\
-             1              0\
-             1      11111   0\
-             1     0        0\
-             0     0  1110000\
-             0     3        0\
-             0   10000      0\
-             0   0   11100  0\
-             0   0   0      0\
-             0   0   1  00000\
-             0       1      0\
-             2       1      0\
-             0       0      0\
-             0 0000000      0\
-             0              0\
-             0002222222200000";
+  canvas.set_draw_color(sdl2::pixels::Color::RGB(33, 33, 33));
+
+  let map = fs::read_to_string("map.txt").unwrap();
+  let map: Vec<char> = map.chars().filter(|ch| *ch != '\n').collect();
 
   let player_x = 3.456_f64;
   let player_y = 2.345_f64;
-  let player_a = 1.523_f64;
-  const fov: f64 = std::f64::consts::PI / 3.0;
+  let mut player_a = 1.523_f64;
+  const FOV: f64 = std::f64::consts::PI / 3.0;
 
   let mut event_pump = sdl_context.event_pump()?;
 
@@ -72,31 +61,22 @@ pub fn main() -> Result<(), String> {
     }
 
     canvas.clear();
-
     // Game loop START
 
-    // Draw bg
-    for j in 0..win_w {
-      for i in 0..win_h {
-        let r = 255.0 * j as f64 / win_h as f64;
-        let g = 255.0 * i as f64 / win_w as f64;
-        let b = 0;
-
-        let offset = j * (3 * 512) + i * 3;
-
-        framebuffer[offset] = r as u8;
-        framebuffer[offset + 1] = g as u8;
-        framebuffer[offset + 2] = b as u8;
-      }
+    // clear framebuffer
+    for elem in framebuffer.iter_mut() {
+      *elem = 0x33;
     }
 
-    let rect_w = win_w / map_w;
+    let rect_w = win_w / (map_w * 2);
     let rect_h = win_h / map_h;
 
     // Draw map
     for j in 0..map_h {
       for i in 0..map_w {
-        if map.chars().nth(i + j * map_w).unwrap() == ' ' {
+        let wall_tile = map[i + j * map_w];
+
+        if wall_tile == ' ' {
           continue;
         }
 
@@ -111,7 +91,7 @@ pub fn main() -> Result<(), String> {
           rect_y,
           rect_w,
           rect_h,
-          sdl2::pixels::Color::RGB(0, 255, 255),
+          wall_tile_to_color(wall_tile),
         );
       }
     }
@@ -128,33 +108,45 @@ pub fn main() -> Result<(), String> {
       sdl2::pixels::Color::RGB(255, 255, 255),
     );
 
-    for i in 0..win_w {
-      let angle = player_a - fov/2.0 + fov * i as f64 / win_w as f64;
+    player_a += 2.0 * std::f64::consts::PI / 360.0;
+
+    // Casting 512 rays
+    for i in 0..win_w / 2 {
+      let angle = player_a - FOV / 2.0 + FOV * i as f64 / (win_w / 2) as f64;
 
       let mut t = 0.0_f64;
       loop {
         let cx = player_x + t * angle.cos();
         let cy = player_y + t * angle.sin();
 
-        if map
-          .chars()
-          .nth(cx as usize + cy as usize * map_w)
-          .unwrap_or('0')
-          != ' '
-        {
-          break;
-        }
-
         let pix_x = (cx * rect_w as f64) as usize;
         let pix_y = (cy * rect_h as f64) as usize;
 
         let offset = (pix_x + pix_y * win_w) * 3;
 
-        framebuffer[offset] = 255;
-        framebuffer[offset + 1] = 255;
-        framebuffer[offset + 2] = 255;
+        framebuffer[offset] = 160;
+        framebuffer[offset + 1] = 160;
+        framebuffer[offset + 2] = 160;
 
-        t = t + 0.05;
+        let wall_hit = map[cx as usize + cy as usize * map_w];
+
+        if wall_hit != ' ' {
+          let column_height = (win_h as f64 / (t * (angle - player_a).cos())) as usize;
+
+          draw_rectangle(
+            &mut framebuffer,
+            win_w,
+            win_h,
+            win_w / 2 + i,
+            win_h / 2 - column_height / 2,
+            1,
+            column_height,
+            wall_tile_to_color(wall_hit),
+          );
+          break;
+        }
+
+        t = t + 0.01;
 
         if t > 20.0 {
           break;
@@ -165,10 +157,10 @@ pub fn main() -> Result<(), String> {
     // Game loop END
 
     texture
-      .update(Rect::new(0, 0, 512, 512), &framebuffer, 3 * 512)
+      .update(Rect::new(0, 0, 1024, 512), &framebuffer, 3 * 1024)
       .unwrap();
 
-    canvas.copy(&texture, None, Some(Rect::new(0, 0, 512, 512)))?;
+    canvas.copy(&texture, None, Some(Rect::new(0, 0, 1024, 512)))?;
     canvas.present();
 
     ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
@@ -192,11 +184,23 @@ fn draw_rectangle(
       let cx = x + i;
       let cy = y + j;
 
+      if cx >= img_w || cy >= img_h {
+        continue; // no need to check for negative values (unsigned variables)
+      };
+
       let offset = (cx + cy * img_w) * 3; // SDL2, 24-bit colour specific tweak: multiply original offset b 3.
 
       framebuffer[offset] = color.r;
       framebuffer[offset + 1] = color.g;
       framebuffer[offset + 2] = color.b;
     }
+  }
+}
+
+fn wall_tile_to_color(wall_char: char) -> sdl2::pixels::Color {
+  match wall_char {
+    '2' => sdl2::pixels::Color::RGB(0, 255, 255),
+    '1' => sdl2::pixels::Color::RGB(255, 0, 255),
+    _ => sdl2::pixels::Color::RGB(255, 255, 0),
   }
 }
